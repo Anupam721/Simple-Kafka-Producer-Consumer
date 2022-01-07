759d47c43b8a39019e3928b7c857704f60f74285
Anupam Dey <anupam97.dey@gmail.com>
2022-01-07 22:53:24 +0530

    Added Kafka Producer folder

---
+++ kafka-producer/src/main/java/com/anupam/kafkaproducer/Controller.java
@@ -0,0 +1,24 @@
+package com.anupam.kafkaproducer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Controller {
+
+    @Autowired
+    KafkaTemplate<String,Book> kafkaTemplate;
+
+    private static final String TOPIC = "NewTopic";
+    
+    @PostMapping("/publish")
+    public String publishMessage(@RequestBody Book book){
+
+        kafkaTemplate.send(TOPIC, book);
+        return "Message Published successfully";
+    }
+}
